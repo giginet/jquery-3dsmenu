8e4523acdcf9a3fbf5fee816208ffd6de85b6638
giginet <giginet.net@gmail.com>
2011-09-25 19:18:34 +0900

    implement expand/contract method.

--- jquery.3dsmenu.js
+++ jquery.3dsmenu.js
@@ -1,25 +1,27 @@
 (function(){
-    $.fn.dsmenu = function(options){
-        options = $.extend(true, {
-            animation : true,
-            animationDuration : 'slow',
-            margin : [3, 3],
-            row : {
-                initial : 3,
-                max : 6,
-                min : 1
-            },
-            resizable : true,
-            scroll : true,
-            sortable : true,
-        }, options);
-
-
-        $(this).each(function(){
-            var $ul = $(this);
-            console.log($(this));
-            var ulHeight = $(this).height();
-            var $cells = $ul.find('li').css({
+    var pluginName = 'dsmenu';
+    var options = {
+        animation : true,
+        animationDuration : 'slow',
+        margin : [3, 3],
+        row : {
+            initial : 3,
+            max : 6,
+            min : 1
+        },
+        resizable : true,
+        scroll : true,
+        sortable : true,
+    };
+    var row = options.row.initial;
+    var $container;
+    var methods = {
+        init : function(custom){
+            if(!custom) custom = {};
+            options = $.extend(true, options, custom);
+            row = options.row.initial;
+            $container = $(this);
+            var $cells = $container.find('li').css({
                 width    : '30px',
                 height   : '30px',
                 position : 'absolute',
@@ -27,55 +29,60 @@
                 right    : 0,
                 bottom   : 0
             });
-
-            var row = options.row.initial;
-            var height = $(this).height();
-
-            $(this).dsmenu.setRow = function(newRow, animation){
-                row = newRow;
-                $cells.each(function(i){
-                    var $cell = $(this);
-                    var c = Math.floor(i / row);
-                    var r = i % row;
-                    var h = (ulHeight - options.margin[0] * (row +1))/row;
-                    var w = h;
-                    console.log(ulHeight);
-                    $cell.width(w);
-                    $cell.height(h);
-                    $cell.position({
-                        of : $ul,
-                        at : 'left top',
-                        my : 'left top',
-                        offset : (options.margin[0] + (w + options.margin[0]) * c) + " " + (options.margin[1] + (h + options.margin[1]) * r),
-                        using : undefined,
-                        collision : "flip flip"
-                    });
+            $(this).dsmenu('setRow', options.row.initial);
+        },
+        setRow : function(newRow, animation){
+            row = newRow;
+            var $ul = $(this);
+            var $cells = $(this).find('li');
+            $cells.each(function(i){
+                $cell = $(this);
+                var c = Math.floor(i / row);
+                var r = i % row;
+                var h = ($container.height() - options.margin[0] * (row +1))/row;
+                var w = h;
+                $cell.width(w);
+                $cell.height(h);
+                $cell.position({
+                    of : $ul,
+                    at : 'left top',
+                    my : 'left top',
+                    offset : (options.margin[0] + (w + options.margin[0]) * c) + " " + (options.margin[1] + (h + options.margin[1]) * r),
+                    using : undefined,
+                    collision : "flip flip"
                 });
+            });
+        },
+        disable : function(){
+        },
+        row : function(){
+            return row;
+        },
+        expand : function(){
+            if(row < options.row.max){
+                var newRow = row + 1;
+                $(this).dsmenu('setRow', newRow);
             }
-
-            $(this).dsmenu.disable = function(){
+        },
+        contract : function(){
+            if(row > Math.max(0, options.row.min)){
+                var newRow = row - 1;
+                $(this).dsmenu('setRow', newRow);
             }
-
-            $(this).dsmenu.row = function(){
-                return row;
-            }
-
-            $(this).dsmenu.expand = function(){
-                if(row < options.row.max){
-                    var newRow = row + 1;
-                    $(this).dsmenu.setRow(newRow);
-                }
-            }
-
-            $(this).dsmenu.contract = function(){
-                if(row > Math.max(0, options.row.min)){
-                    var newRow = row - 1;
-                    $(this).dsmenu.setRow(newRow);
-                }
-            }
-
-            $(this).dsmenu.setRow(options.row.initial);
-
-        });
+        }
     };
+
+    $.fn[pluginName] = function(method) {
+        if (method) {
+            return methods[method]
+            .apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' +  method + ' does not exist on jQuery.' + plugname );
+            return this;
+        }
+    };
+
+
 })(jQuery);
