6d4600db4351ae3576fe60e70267733277cb9bcc
giginet <giginet.net@gmail.com>
2011-09-25 16:26:03 +0900

    use jQueryUI

--- jquery.3dsmenu.js
+++ jquery.3dsmenu.js
@@ -1,8 +1,35 @@
 (function(){
-    $.fn.dsmenu = function(){
+    $.fn.dsmenu = function(options){
+        options = $.extend(true, {
+            animation : true,
+            animationDuration : 'slow',
+            row : {
+                initial : 3,
+                max : 6,
+                min : 1
+            },
+            resizable : true,
+            scroll : true,
+            sortable : true,
+        }, options);
         $(this).each(function(){
             $ul = $(this);
-            console.log("Hello");
+            $li = $ul.find('li').css({
+                width: '75px',
+                height: '75px',
+                position: 'absolute',
+                display: 'block',
+                right: 0,
+                bottom: 0
+            });
+            $li.position({
+                of : $ul,
+                at : 'left top',
+                my : 'left top',
+                offset : '100 100',
+                using : undefined,
+                collision : "flip flip"
+            });
         });
     };
 })(jQuery);
